5c9d1202e4eff8ce79d3724eb30e69942341883e
Minghao Wang <minghao.wang@thoughtworks.com>
2022-06-01 15:07:56 +0800

    [refactor] add website and dao

---
+++ Exercise1-JDBC/src/main/java/com/tw/exercise/domain/Website.java
@@ -0,0 +1,70 @@
+package com.tw.exercise.domain;
+
+public class Website {
+
+    private Long id;
+    private String name;
+    private String url;
+    private Integer alexa;
+    private String country;
+
+    public Website(Long id, String name, String url, Integer alexa, String country) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.alexa = alexa;
+        this.country = country;
+    }
+
+    @Override
+    public String toString() {
+        return "Website{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                ", alexa=" + alexa +
+                ", country='" + country + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Integer getAlexa() {
+        return alexa;
+    }
+
+    public void setAlexa(Integer alexa) {
+        this.alexa = alexa;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+}
